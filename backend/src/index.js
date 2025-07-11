5a3f01a6a710ace7f1502a83e2987a4edac69ac6
vashista <82438452+vashista143@users.noreply.github.com>
2025-07-11 23:30:00 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -89,8 +89,9 @@ app.use(passport.session());
         }
       });
     });
+const PORT = process.env.PORT || 5000;
 
-    server.listen(5000, () => {
+    server.listen(PORT, () => {
       console.log("Server running at http://localhost:5000");
     });
   }
