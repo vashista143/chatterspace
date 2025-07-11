863ac91306e35eee2f57c73814da2582ebd770df
vashista <82438452+vashista143@users.noreply.github.com>
2025-07-12 00:34:29 +0500

    Update SignupPage.jsx

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -59,7 +59,7 @@ const onsubmit = async (data) => {
   }
 
   try {
-    const response = await fetch("http://localhost:5000/api/auth/signup", {
+    const response = await fetch(`${import.meta.env.VITE_backendurl}/api/auth/signup`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
