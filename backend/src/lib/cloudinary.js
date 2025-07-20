6737efb77de7eba12043d71fdab74f060a47f54c
vashista <82438452+vashista143@users.noreply.github.com>
2025-07-20 10:08:11 +0500

    Delete backend directory

--- backend/src/lib/cloudinary.js
+++
@@ -1,8 +0,0 @@
-import {v2 as cloudinary} from "cloudinary"
-cloudinary.config({
-    cloud_name: "dbvwth3yk",
-    api_key: "775586472177249", 
-    api_secret: "DIG10_R_l942snDsupoHHN3KPqM"}
-)
- 
-export default cloudinary;
