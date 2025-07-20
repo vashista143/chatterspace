f983dd0bea278a0e2012b203ef1581f3fd196d6a
vashista <82438452+vashista143@users.noreply.github.com>
2025-07-20 11:26:40 +0500

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -76,8 +76,8 @@ export const login =async (req,res)=>{
         res.cookie("jwt", token, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
-        secure: false,
+        sameSite: "None",
+        secure: true,
         });
 
         return res.status(201).json({
