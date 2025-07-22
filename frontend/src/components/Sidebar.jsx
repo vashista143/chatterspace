bec10b8fa70f95494b0d38d84c522e48427d334d
vashista <82438452+vashista143@users.noreply.github.com>
2025-07-22 05:00:57 +0500

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { forwardRef } from "react";
 import { useEffect } from "react";
-const Sidebar = ({ sidebarusers, selecteduser, onlineUsers, setselecteduser }) => {
-    return (
-    <div className="md:w-80 w-40 h-full bg-base-200 text-white border-r border-white/10 md:pl-4 pl-1 pb-4 pr-2 pt-4 flex flex-col">
+const Sidebar = forwardRef((props, ref) => {
+  const { sidebarusers, selecteduser, onlineUsers, setselecteduser } = props;
+return (
+    <div id="sidebarcomp" ref={ref} className="md:w-80 w-40 h-full bg-base-200 text-white border-r border-white/10 md:pl-4 pl-1 pb-4 pr-2 pt-4 flex flex-col">
   <h2 className="text-xl font-semibold mb-4">Messages</h2>
   <div className="flex-1 overflow-y-auto pr-2 space-y-2 scrollbar-thin scrollbar-thumb-yellow-500 scrollbar-track-neutral-800">
     {sidebarusers?.length > 0 ? (
@@ -14,6 +16,9 @@ const Sidebar = ({ sidebarusers, selecteduser, onlineUsers, setselecteduser }) =
             onClick={() => {
               setselecteduser(null);
               setselecteduser(user);
+              if (window.innerWidth <= 768 && ref?.current) {
+ref.current.classList.add("hidden");
+                  }
             }}
             className={`flex items-center gap-3 md:p-2 p-1 rounded-lg cursor-pointer transition ${
               isSelected ? "bg-neutral-800" : "hover:bg-neutral-800"
@@ -41,6 +46,6 @@ const Sidebar = ({ sidebarusers, selecteduser, onlineUsers, setselecteduser }) =
 </div>
 
   );
-};
+});
 
 export default Sidebar;
